9b80de4beac5873f6a048a9ea77e200417c9f85c
Larry Kagan <larry@superiocity.com>
2022-08-29 13:07:49 -0400

    Create custom error classes

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,18 @@
-import {Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log('Something went wrong', err);
-    res.status(400).send({message: 'Something went wrong'});
+export const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (err instanceof RequestValidationError) {
+        console.log('Handling this error as a request validation error');
+    } else if (err instanceof DatabaseConnectionError) {
+        console.log('Handling this error as a DB connection error');
+    }
+
+    res.status(400).send({ message: 'Something went wrong' });
 };
