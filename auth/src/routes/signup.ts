49dbe9e0e864ecb22c48de25971628be58fae79d
Larry Kagan <larry@superiocity.com>
2022-09-13 15:58:59 -0400

    Fixed bug in throwing exceptions from async routes

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
@@ -14,18 +14,18 @@ router.post('/api/users/signup', [
         .trim()
         .isLength({ min: 4, max: 20 })
         .withMessage('Password must be between 4 and 20 characters')
-], async (req: Request, res: Response) => {
+], async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
+        return next(new RequestValidationError(errors.array()));
     }
 
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-        throw new BadRequestError('Email in use');
+        return next(new BadRequestError('Email in use'));
     }
 
     const user = User.build({ email, password });
