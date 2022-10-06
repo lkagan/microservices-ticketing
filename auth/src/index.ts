fa7fa3e00723becc36a4e3cf98fd6ed642e62615
Larry Kagan <larry@superiocity.com>
2022-10-05 21:28:29 -0400

    Enable the use of cookies via cookie-session package

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,9 +7,15 @@ import { signupRouter } from "./routes/signup";
 import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handler";
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(cookieSession({
+   signed: false,
+   secure: true,
+}));
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
